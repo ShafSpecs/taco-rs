3e7a0cabbd9e9c230bea3676755f125e69b9148b
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-18 17:45:13 +0100

    chore: basic token system

---
+++ src/parser/tokens.rs
@@ -0,0 +1,77 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Integer,
+    Float,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Func,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Let,
+    While,
+
+    Eof,
+}
+
+#[derive(Clone, Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub literal: String,
+    pub line: i32,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: String, line: i32) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+
+    pub fn to_string(&mut self) -> String {
+        return String::from(format!(
+            "{:#?}: {} - {}",
+            self.token_type, self.lexeme, self.literal
+        ));
+    }
+}
