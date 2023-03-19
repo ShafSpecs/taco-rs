8f3f5457dd3fc7a54902f18833255227e1fd99ec
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-19 17:20:42 +0100

    chore: defined main expression

---
+++ src/core/expression.rs
@@ -0,0 +1,12 @@
+use crate::core::binary::BinaryExpr;
+use crate::core::grouping::GroupingExpr;
+use crate::core::literal::Literal;
+use crate::core::unary::UnaryExpr;
+
+pub enum Expr {
+    BinaryExpr(Box<BinaryExpr>),
+    GroupingExpr(Box<GroupingExpr>),
+    UnaryExpr(Box<UnaryExpr>),
+    // LiteralExpr(Box<LiteralExpr<Box<dyn Any>>>)
+    Literal(Literal),
+}
