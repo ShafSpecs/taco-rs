648357a6ae9bd296bacdbe55c62504441f3d029c
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-20 11:33:32 +0100

    refactor: explicitly added return types

--- src/error/handling.rs
+++ src/error/handling.rs
@@ -1,7 +1,7 @@
-pub fn report(line: u8, where_: &str, message: &str) {
+pub fn report(line: u8, where_: &str, message: &str) -> () {
   eprintln!("[Line {}] Error {}: {}", line, where_, message);
 }
 
-pub fn error(line: u8, message: &str) {
+pub fn error(line: u8, message: &str) -> () {
   report(line, "", message);
 }
