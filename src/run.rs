3dfd3bbdab50fa04b14115ff9204851354234c52
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-20 11:32:37 +0100

    chore: print ast output from parser

--- src/run.rs
+++ src/run.rs
@@ -1,7 +1,11 @@
-use crate::token::scanner::Scanner;
+use crate::{token::scanner::Scanner, parser::parser::Parser};
 
 pub fn exec(source: String) {
   let mut scanner = Scanner::new(source);
   let tokens = scanner.scan_tokens();
-  println!("{:#?}", tokens)
+  let mut parser = Parser::new(tokens.clone());
+  let expr = parser.parse();
+
+  println!("{:?}\n\n", tokens.clone());
+  println!("{:?}", expr.unwrap())
 }
