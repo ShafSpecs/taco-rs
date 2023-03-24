343dd78402dd3e79faa513e4d8e4f70fec7b0c6f
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-24 19:51:13 +0100

    fix: fixing up language struct

--- src/lang/taco.rs
+++ src/lang/taco.rs
@@ -1,28 +1,60 @@
 use std::fs::read;
 
-use rustyline::{DefaultEditor, error::ReadlineError};
+use rustyline::{error::ReadlineError, DefaultEditor};
 
-use crate::run::exec;
+use crate::{interpreter::interpreter::Interpreter, run::exec, parser::parser::Parser};
 
 pub struct Taco {
     pub has_error: bool,
+    pub has_runtime_error: bool,
+    pub interpreter: Option<Interpreter>,
+    pub parser: Option<Parser>
 }
 
 impl Taco {
     pub fn new() -> Taco {
-        Taco { has_error: false }
+        Taco {
+            has_error: false,
+            has_runtime_error: false,
+            interpreter: None,
+            parser: None
+        }
     }
 
-    pub fn run_file(path: String) {
+    pub fn interpreter(&mut self) -> &mut Interpreter {
+        if self.interpreter.is_none() {
+            self.interpreter = Some(Interpreter::new(self));
+        }
+
+        self.interpreter.as_mut().unwrap()
+    }
+
+    pub fn set_error (&mut self, value: bool) {
+        self.has_error = value;
+    }
+
+    pub fn set_runtime_error (&mut self, value: bool) {
+        self.has_runtime_error = value;
+    }
+
+    pub fn run_file(&mut self, path: String) {
         let bytes = read(path).unwrap();
 
         let source = String::from_utf8(bytes).unwrap();
 
-        exec(source);
-        todo!("Remove the code splurging and handle the error better");
+        exec(source, self);
+
+        if self.has_error {
+            std::process::exit(65);
+        }
+        if self.has_runtime_error {
+            std::process::exit(70);
+        }
+
+        todo!("Remove the code splurging (in `exec`) and handle the error better");
     }
 
-    pub fn run_repl() {
+    pub fn run_repl(&mut self) {
         let mut rl = DefaultEditor::new().unwrap();
 
         // Used to create an infinite loop
@@ -30,7 +62,8 @@ impl Taco {
             let readline = rl.readline(">> ");
             match readline {
                 Ok(line) => {
-                    exec(line);
+                    exec(line, self);
+                    self.has_error = false;
                 }
                 Err(ReadlineError::Interrupted) => break,
                 Err(ReadlineError::Eof) => break,
