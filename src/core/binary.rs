7a0fb48eb801152bc783c4d1ae4969a9db0222ca
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-24 19:46:24 +0100

    implemented getters

--- src/core/binary.rs
+++ src/core/binary.rs
@@ -1,6 +1,6 @@
 use crate::{core::expression::Expr, token::tokens::Token, into_expr};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BinaryExpr {
   left: Expr,
   operator: Token,
@@ -12,5 +12,17 @@ into_expr!(BinaryExpr);
 impl BinaryExpr {
   pub fn new(left: Expr, operator: Token, right: Expr) -> BinaryExpr {
     BinaryExpr { left, operator, right }
+  }
+
+  pub fn get_left(&self) -> Expr {
+    self.left.clone()
+  }
+
+  pub fn get_operator(&self) -> &Token {
+    &self.operator
+  }
+
+  pub fn get_right(&self) -> Expr {
+    self.right.clone()
   }
 }
