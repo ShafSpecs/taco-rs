cb54e38475dd3da9809127b5f7558c1e08d65f63
Abdur-Rahman <abbadshaf05@gmail.com>
2023-03-27 23:00:21 +0100

    refactor: OMG

--- src/run.rs
+++ src/run.rs
@@ -1,41 +1,44 @@
-use std::ops::Deref;
+use std::os::windows::process;
 
-use crate::{token::scanner::Scanner, parser::parser::Parser, lang::taco::Taco, error::handling::report};
+use crate::{
+    error::interpreter::throw_runtime_error, lang::taco::Taco, parser::parser::Parser,
+    token::scanner::Scanner,
+};
 
 pub fn exec(source: String, lang: &mut Taco) {
-  let mut scanner = Scanner::new(source);
-  let tokens = scanner.scan_tokens();
-  let mut parser = Parser::new(tokens.clone());
-  let expr = parser.parse();
+    let mut scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+    let mut parser = Parser::new(tokens.clone());
+    let expr = parser.parse();
 
-  match expr {
-    Ok(_) => (),
-    Err(e) => {
-      // `throw_error` should handle this. 
-      // eprintln!("{}", e);
-      lang.set_error(true);
-      return;
+    match expr {
+        Ok(_) => (),
+        Err(_e) => {
+            lang.set_error(true);
+            // std::process::exit(65);
+            return;
+        }
     }
-  }
 
-  let err = lang.interpreter().interpret(expr.clone());
+    let err = lang.interpreter().interpret(expr.clone());
 
-  match err {
-      Ok(_) => {
-        if lang.has_error {
-          std::process::exit(65);
-        };
+    match err {
+        Ok(_) => {
+            if lang.has_error {
+                // std::process::exit(65);
+                return;
+            };
 
-        ()
-      },
-      Err(e) => {
-          report(e.get_token().line as u8, format!(" at '{}'", e.get_token().lexeme).as_str(), e.get_message());
-          lang.set_runtime_error(true);
-          return;
-      }
-  }
+            ()
+        }
+        Err(e) => {
+            throw_runtime_error(e);
+            lang.set_runtime_error(true);
+            // std::process::exit(70);
+            return;
+        }
+    }
 
-  println!("{:?}\n\n", tokens.clone());
-  println!("{:?}\n\n", expr.clone().unwrap());
-  // println!("{:?}\n\n", lang.interpreter().environment.clone());
+    // println!("{:?}\n\n", tokens.clone());
+    // println!("{:?}\n\n", expr.clone().unwrap());
 }
